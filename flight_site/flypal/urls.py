4ce2425e3f5e5922ed2dea89ba72eb28e4256b47
Cessaprince <cessaprince2007@gmail.com>
2024-11-06 11:36:26 +0100

    Made changes to flight_site

--- flight_site/flypal/urls.py
+++ flight_site/flypal/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page_view, name='home')
+    path('home_page/', views.home_page_view, name='home_page'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- flight_site/flypal/views.py
+++ flight_site/flypal/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page_view(request):
-    return HttpResponse('This is the home page')
+    return render(request, 'flypal/home_page.html')
+
+
+def login_view(request):
+    return render(request, 'flypal/login.html')
+
+
+def register_view(request):
+    return render(request, 'flypal/register.html')
